c60f0e8ce1c0c76594279e9b75672dce26d3e651
Bryan Villalon <bavillalon@gmail.com>
2019-09-10 18:51:31 -0500

    makes connection, gets a list of products, and makes purchases. also exits on demand and goes back to the home page and allows the user to review quantities if they try to buy too much.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,49 +1,118 @@
-var mysql=require('mysql');
-var inquirer=require('inquirer');
+var mysql = require('mysql');
+var inquirer = require('inquirer');
 
-var connection=mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'password',
-    database:'bamazon'
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'bamazon'
 });
 
-function startBamazon(){
-    var productsArray=[];
-    connection.query('SELECT products.product_name, products.item_id FROM products', function(err,res){
-        if(err) throw err;
-        for(let i=0; i<res.length;i++){
-            productsArray[i]={name:res[i].product_name, value:res[i].item_id};
+function startBamazon() {
+    var productsArray = [{ name: "Exit Bamazon", value: 0 }];
+    connection.query('SELECT products.product_name, products.price, products.item_id FROM products', function (err, res) {
+        if (err) throw err;
+        for (let i = 0; i < res.length; i++) {
+            productsArray.push({
+                name: `${res[i].item_id.toString()}: "${res[i].product_name}" Priced at $${res[i].price}`,
+                value: res[i].item_id
+            });
         };
+        console.log("*********************************************");
+        console.log("Welcome to Bamazon!");
+        console.log("*********************************************");
+        console.log("");
         inquirer.prompt([
             {
-                type:'list',
-                name:'item_id',
-                message:'Please select a product from the list below...',
+                type: 'list',
+                name: 'item_id',
+                message: 'Please select a product from the list below...',
                 choices: productsArray
+            },
+            {
+                type: 'number',
+                name: 'quantity',
+                message: 'How many would you like to purchase? (type 0 if you want to go back to the store)'
             }
-        ]).then(function(response){
-            console.log(response);
-            connection.end();
+        ]).then(function (response) {
+            if (response.item_id > 0) {
+                if (typeof (response.quantity) === 'number') {
+                    if (response.quantity > 0) {
+                        buySomething(response);
+                    }
+                    else {
+                        startBamazon();
+                    }
+                }
+                else {
+                    console.log("Please enter a number");
+                    startBamazon();
+                }
+            }
+            else {
+                connection.end();
+            }
         });
         //console.log(res);
     });
-
- //   connection.query('SELECT products.product_name, products.item_id FROM products', function(err,res){
- //       if(err) throw err;
- //       console.log(res);
- //       connection.end();
- //   });
-
 };
 
-connection.connect(function(err) {
+function buySomething(productObject) {
+    var customerQuantity = productObject.quantity;
+    connection.query('SELECT products.stock_quantity, products.price, products.product_name FROM products WHERE ?', { item_id: productObject.item_id }, function (err, res) {
+        if (err) throw err;
+        var cost = 0;
+        console.log("*********************************************");
+        console.log(`You want to purchase ${customerQuantity} units of ${res[0].product_name} at ${res[0].price} per unit.`);
+        if (customerQuantity <= res[0].stock_quantity) {
+            cost = res[0].price * customerQuantity;
+            connection.query('UPDATE products SET ? WHERE ?', [
+                { stock_quantity: res[0].stock_quantity - customerQuantity },
+                { item_id: productObject.item_id }
+            ], function (err) {
+                if (err) throw err;
+                console.log("You're in luck! We have enough.")
+                console.log(`Order Placed! Your cost was ${cost}`);
+                console.log("*********************************************");
+                connection.end();
+            });
+        }
+        else {
+            console.log(`The quantity you requested is too high. There are only ${res[0].stock_quantity} in stock.`)
+            inquirer.prompt([
+                {
+                    type: 'number',
+                    name: 'update',
+                    message: 'How many would you like to purchase? (type 0 if you want to go back to the store)'
+                }
+            ]).then(function (response) {
+                if(response.update>0){
+                    productObject.quantity = response.update;
+                    buySomething(productObject);
+                }
+                else{
+                    startBamazon();
+                };
+            });
+        };
+    });
+};
+
+//   connection.query('SELECT products.product_name, products.item_id FROM products', function(err,res){
+//       if(err) throw err;
+//       console.log(res);
+//       connection.end();
+//   });
+
+
+
+connection.connect(function (err) {
     if (err) {
-      console.error('error connecting: ' + err.stack);
-      return;
+        console.error('error connecting: ' + err.stack);
+        return;
     }
     console.log('connected as id ' + connection.threadId);
     startBamazon();
-   // connection.end();
-  });
+    // connection.end();
+});
 
